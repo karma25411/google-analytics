804c731a9e0974ed711364343f8d77cd78b1307a
karma lama <karma@pacecode.com.np>
2025-04-23 20:15:42 +0545

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleTagManager gtmId="GTM-XYZ" />
+        <GoogleAnalytics gaId="G-98GCRCK4K3" />
         {children}
       </body>
     </html>
